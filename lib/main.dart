7cd0ff5abb14c8033eb5640ee69cb5fdfc663192
Amina-UC <aminafathimauc20@.com>
2021-03-14 15:42:53 +0530

    odexxo location tracking code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart' as p;
 import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -22,7 +26,8 @@ class MapPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => MapPageState();
 }
-
+String googleAPIKey = 'AIzaSyABBH07rVnsmRDsmIjtpfHiBuwczmyVyLk';
+p.GoogleMapsPlaces _places = p.GoogleMapsPlaces(apiKey: googleAPIKey);
 class MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _markers = Set<Marker>();
@@ -30,15 +35,15 @@ class MapPageState extends State<MapPage> {
   Set<Polyline> _polylines = Set<Polyline>();
   List<LatLng> polylineCoordinates = [];
   PolylinePoints polylinePoints;
-  String googleAPIKey = 'AIzaSyAm22_7Hj0aHN09DvP-VmaXSk64dm1WSfg';
+
 // for my custom marker pins
   BitmapDescriptor sourceIcon;
   BitmapDescriptor destinationIcon;
 // the user's initial location and current location
 // as it moves
-  LocationData currentLocation;
+  LatLng currentLocation;
 // a reference to the destination location
-  LocationData destinationLocation;
+  LatLng destinationLocation;
 // wrapper around the location API
   Location location;
   double pinPillPosition = -100;
@@ -54,7 +59,7 @@ class MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
-
+    getAssigningStaffList();
     // create an instance of Location
     location = new Location();
     polylinePoints = PolylinePoints();
@@ -65,7 +70,8 @@ class MapPageState extends State<MapPage> {
       // cLoc contains the lat and long of the
       // current user's position in real time,
       // so we're holding on to it
-      currentLocation = cLoc;
+      // currentLocation = LatLng(cLoc.latitude,cLoc.longitude);
+      currentLocation = LatLng(9.9691,76.3217);
       updatePinOnMap();
     });
     // set custom marker pins
@@ -84,6 +90,24 @@ class MapPageState extends State<MapPage> {
     getBytesFromAsset('assets/image/destination_map_marker.png', 64).then((onValue) {
       customIcon4 =BitmapDescriptor.fromBytes(onValue);
     });
+  }
+
+  Future getAssigningStaffList() async {
+    final response = await http.get(
+      //https://carecanadajobs.com/public/api/staffassigntickets?ticket_id=47
+      Uri.encodeFull("https://oddexo.com/oddexo/services/API/autoServiceApi.php?service=driverLocation&driverid=856"),
+    );
+    var qData = json.decode(response.body);
+    print("dxdcfgvbhkjnkm "+qData.toString());
+    setState(() {
+      // currentLocation = LatLng(qData["latitude"],qData["longitude"]);
+      currentLocation = LatLng(9.9691,76.3217);
+      destinationLocation = LatLng(9.9682, 76.318);
+    });
+    setState(() {
+      _markers.clear();
+    });
+    showPinsOnMap();
   }
 
   static Future<Uint8List> getBytesFromAsset(String path, int width) async {
@@ -108,21 +132,63 @@ class MapPageState extends State<MapPage> {
   }
 
   void setInitialLocation() async {
+   /* Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.DENIED) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.DENIED) {
+        return;
+      }
+    }*/
     // set the initial location by pulling the user's
     // current location from the location's getLocation()
-    currentLocation = await location.getLocation();
+    /*setState(() async{
+      // currentLocation = await location.getLocation();
+    });*/
 
     // hard-coded destination for this example
-    destinationLocation = LocationData.fromMap({
-      "latitude": DEST_LOCATION.latitude,
-      "longitude": DEST_LOCATION.longitude
-    });
+    destinationLocation = DEST_LOCATION;
   }
 
   BitmapDescriptor customIcon1 ;
   BitmapDescriptor customIcon2 ;
   BitmapDescriptor customIcon3;
   BitmapDescriptor customIcon4 ;
+
+  TextEditingController _loc = TextEditingController();
+
+  Future<Null> displayPrediction(p.Prediction pp, ScaffoldState scaffold) async {
+    if (pp != null) {
+      // get detail (lat/lng)
+      p.PlacesDetailsResponse detail =
+      await _places.getDetailsByPlaceId(pp.placeId);
+      setState(() {
+        destinationLocation = LatLng(detail.result.geometry.location.lat,detail.result.geometry.location.lng);
+      });
+      print("llllllaaaaaaaaattttttt %%%%%%%%%%%%%%%%%%% "+destinationLocation.toString());
+      print("llllllaaaaaaaaattttttt %%%%%%%%%%%%%%%%%%% &&&&&&&&&&&&&&&&"+detail.result.geometry.location.toString());
+      print("llllllaaaaaaaaattttttt "+detail.result.geometry.location.lat.toString());
+      print("llllllongggg "+detail.result.geometry.location.lng.toString());
+      setState(() {
+        _markers.clear();
+      });
+      showPinsOnMap();
+    }
+  }
+  final homeScaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
@@ -138,37 +204,103 @@ class MapPageState extends State<MapPage> {
           tilt: CAMERA_TILT,
           bearing: CAMERA_BEARING);
     }
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            GoogleMap(
-                myLocationEnabled: true,
-                compassEnabled: true,
-                tiltGesturesEnabled: false,
-                markers: _markers,
-                polylines: _polylines,
-                mapType: MapType.normal,
-                initialCameraPosition: initialCameraPosition,
-                onTap: (LatLng loc) {
-                  pinPillPosition = -100;
-                },
-                onMapCreated: (GoogleMapController controller) {
-                  controller.setMapStyle(Utils.mapStyles);
-                  _controller.complete(controller);
-                  // my map has completed being created;
-                  // i'm ready to show the pins on the map
-                  showPinsOnMap();
-                }),
-            MapPinPillComponent(
-                pinPillPosition: pinPillPosition,
-                currentlySelectedPin: currentlySelectedPin)
-          ],
+      appBar: AppBar(
+        title: Text("Location Tracking"),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _loc,
+
+                  readOnly: true,
+                  onTap: () async {
+                    p.Prediction pat = await PlacesAutocomplete.show(
+                      context: context,
+                      apiKey: googleAPIKey,
+                      onError: (error){
+                        print("errrooorrr "+error.status);
+                        print("errrooorrr msg"+error.errorMessage);
+                      },
+                      mode: Mode.overlay,
+                      // Mode.fullscreen
+                      language: "IN",
+                      components: [
+                        new p.Component(p.Component.country, "IN")
+                      ],
+
+                    );
+                    displayPrediction(pat, homeScaffoldKey.currentState);
+                    _loc.text = pat.description;
+                  },
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Colors.grey[300],
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                            color: Colors.transparent
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                            color: Colors.transparent
+                        )
+                    ),
+                    hintText: "Location",
+                    hintStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.location_on,color: Colors.black,size: 22,),
+                    suffixIcon: GestureDetector(
+                        onTap: (){
+
+                        },
+                        child: Icon(Icons.gps_fixed,color: Colors.black,size: 22,)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(height: height-200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black)
+                  ),
+                  child: GoogleMap(
+                      myLocationEnabled: false,
+                      compassEnabled: true,
+                      tiltGesturesEnabled: false,
+                      markers: _markers,
+                      polylines: _polylines,zoomGesturesEnabled: true,
+                      mapType: MapType.terrain,
+                      initialCameraPosition: initialCameraPosition,
+                      onTap: (LatLng loc) {
+                        pinPillPosition = -100;
+                      },
+                      onMapCreated: (GoogleMapController controller) {
+                        controller.setMapStyle(Utils.mapStyles);
+                        _controller.complete(controller);
+                        // my map has completed being created;
+                        // i'm ready to show the pins on the map
+                        showPinsOnMap();
+                      }),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
+/*MapPinPillComponent(
+                    pinPillPosition: pinPillPosition,
+                    currentlySelectedPin: currentlySelectedPin)*/
   void showPinsOnMap() {
     // get a LatLng for the source location
     // from the LocationData currentLocation object
@@ -180,14 +312,14 @@ class MapPageState extends State<MapPage> {
 
     sourcePinInfo = PinInformation(
         locationName: "Start Location",
-        location: SOURCE_LOCATION,
+        location: LatLng(currentLocation.latitude,currentLocation.longitude),
         pinPath: "assets/image/driving_pin.png",
         avatarPath: "assets/image/friend1.jpg",
-        labelColor: Colors.blueAccent);
+        labelColor: Colors.greenAccent);
 
     destinationPinInfo = PinInformation(
         locationName: "End Location",
-        location: DEST_LOCATION,
+        location: destinationLocation,
         pinPath: "assets/image/destination_map_marker.png",
         avatarPath: "assets/image/friend2.jpg",
         labelColor: Colors.purple);
@@ -228,15 +360,19 @@ class MapPageState extends State<MapPage> {
         destinationLocation.longitude);
 
     if (result.isNotEmpty) {
+      setState(() {
+        polylineCoordinates.clear();
+      });
       result.forEach((PointLatLng point) {
         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
       });
 
       setState(() {
+        _polylines.clear();
         _polylines.add(Polyline(
-            width: 2, // set the width of the polylines
+            width: 3, // set the width of the polylines
             polylineId: PolylineId("poly"),
-            color: Color.fromARGB(255, 40, 122, 198),
+            color: Colors.yellow[600],
             points: polylineCoordinates));
       });
     }
@@ -258,11 +394,8 @@ class MapPageState extends State<MapPage> {
     // that a widget update is due
     setState(() {
       // updated position
-      var pinPosition =
-      LatLng(currentLocation.latitude, currentLocation.longitude);
-
+      var pinPosition = LatLng(currentLocation.latitude, currentLocation.longitude);
       sourcePinInfo.location = pinPosition;
-
       // the trick is to remove the marker (by id)
       // and add it again at the updated location
       _markers.removeWhere((m) => m.markerId.value == 'sourcePin');
